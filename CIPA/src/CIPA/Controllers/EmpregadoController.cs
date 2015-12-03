0d1eea1ef09d5e64f807c097f6f3fa9fca087b91
Victor Hugo <xblader@gmail.com>
2015-12-03 21:23:03 -0200

    com erro de entry point

---
+++ CIPA/src/CIPA/Controllers/EmpregadoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using CIPA.Persistence;
+using CIPA.Domain;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CIPA.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmpregadoController : Controller
+    {
+        [FromServices]
+        public IRepository<Empregado> RepoEmpregado { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Empregado> GetAll()
+        {
+            return RepoEmpregado.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetEmpregado")]
+        public IActionResult GetById(string id)
+        {
+            var item = RepoEmpregado.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
