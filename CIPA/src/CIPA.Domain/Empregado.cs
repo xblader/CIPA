d4fd62a0233f36abe62750325a153db5210514b4
Victor Hugo <xblader@gmail.com>
2015-12-02 22:23:30 -0200

    com entitybase

--- CIPA/src/CIPA.Domain/Empregado.cs
+++ CIPA/src/CIPA.Domain/Empregado.cs
@@ -7,7 +7,7 @@ namespace CIPA.Domain
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Empregado
+    public class Empregado : EntityBase
     {
         public string Nome { get; set; }
         public string Endereco { get; set; }
