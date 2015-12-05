209ef90680590d56be609fd048177f2e511a7fcd
Victor Hugo <xblader@gmail.com>
2015-12-04 23:36:17 -0200

    CRUD para empregado

--- CIPA/src/CIPA/Controllers/EmpregadoController.cs
+++ CIPA/src/CIPA/Controllers/EmpregadoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using CIPA.Persistence;
 using CIPA.Domain;
+using CIPA.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -31,6 +32,41 @@ namespace CIPA.Controllers
                 return HttpNotFound();
             }
             return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Empregado item)
+        {
+            if (item == null)
+            {
+                return HttpBadRequest();
+            }
+            RepoEmpregado.Add(item);
+            return CreatedAtRoute("GetEmpregado", new { controller = "Empregado", id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] Empregado item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return HttpBadRequest();
+            }
+
+            var todo = RepoEmpregado.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+
+            RepoEmpregado.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            RepoEmpregado.Remove(id);
         }
     }
 }
